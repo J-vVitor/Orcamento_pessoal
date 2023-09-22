a8554b9b1347af2cd764b3768fb35f3b66f61abc
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-09-21 21:31:06 -0300

    Em andamento...

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,27 @@
-function teste(){
+function teste() {
     var consulta = document.querySelector('#consulta');
 
-    if(consulta.style.display == 'none'){
+    if (consulta.style.display == 'none') {
         consulta.style.display = 'block'
-    }else{
+    } else {
         consulta.style.display = 'none'
     }
 }
 
-function envio(){
+
+class Despesas {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+            this.ano = ano
+            this.mes = mes
+            this.dia = dia
+            this.tipo = tipo
+            this.descricao = descricao
+            this.valor = valor
+    }
+}
+
+function cadastrarDespesas() {
+
     let ano = document.querySelector('#ano').value
     let mes = document.querySelector('#mes').value
     let dia = document.querySelector('#dia').value
@@ -16,6 +29,15 @@ function envio(){
     let descricao = document.querySelector('#descricao').value
     let valor = document.querySelector('#valor').value
 
-    alert(valor)
-  
+    let despesas = new Despesas(ano, mes, dia, tipo, descricao, valor)
+
+    gravar(despesas)
+
 }
+
+function gravar(d){
+    localStorage.setItem('despesas',JSON.stringify(d))
+}
+
+
+
