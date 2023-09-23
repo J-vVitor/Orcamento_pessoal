700e3968cdb1e396a5c591ab0e2d968568e01997
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-09-23 20:16:18 -0300

    complementos

--- assets/script.js
+++ assets/script.js
@@ -10,33 +10,76 @@ function teste() {
 
 let valor = document.querySelector("#valor")
 
-valor.addEventListener('keypress', () =>{
+/*valor.addEventListener('keypress', () => {
     let inputLength = valor.value.length
 
     if(inputLength === 3){
         valor.value += ','
     }
-})
+})*/
 
 
 
 
 class Despesas {
     constructor(ano, mes, dia, tipo, descricao, valor) {
-            this.ano = ano
-            this.mes = mes
-            this.dia = dia
-            this.tipo = tipo
-            this.descricao = descricao
-            this.valor = valor
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+
+    validarDdos() {
+        for (let i in this) {
+            if (this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            }
+        }
+        return true
     }
 }
 
 class Bd {
-    gravar(d){
-        localStorage.setItem('despesas',JSON.stringify(d))
+    constructor() {
+        let id = localStorage.getItem('id')
+        if (id === null) {
+            localStorage.setItem('id', 0)
+
+        }
     }
+
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d) {
+        let id = this.getProximoId()
+        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem('id', id)
+    }
+
+    recuperarTodosRegistros() {
+
+        let despesas = Array()
+
+        let id = JSON.parse(localStorage.getItem('id'))
+
+        for (let i = 1; i <= id; i++) {
+            let despesa = localStorage.getItem(i)
+
+            despesas.push(despesa)
+        }
+
+        console.log(despesas)
+    }
+
+
 }
+
 
 let bd = new Bd()
 
@@ -52,7 +95,45 @@ function cadastrarDespesas() {
     let despesas = new Despesas(ano, mes, dia, tipo, descricao, valor)
 
 
-    bd.gravar(despesas)
+    if (despesas.validarDdos()) {
+        bd.gravar(despesas)
+
+        testeButton.valido()
+    } else {
+        testeButton.erro()
+    }
+
+
 }
+
+
+class TesteButton {
+    valido() {
+        Swal.fire({
+            icon: 'success',
+            title: 'Novo Registro',
+            text: 'Registro adicionado com sucesso!',
+            showCloseButton: 'close'
+        })
+    }
+
+    erro() {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Existe campos obrigatórios que não foram preenchidos',
+            showCloseButton: 'close'
+        })
+    }
+}
+
+let testeButton = new TesteButton()
+
+function carregarTodasDespesas() {
+    bd.recuperarTodosRegistros()
+}
+
+let resultado = document.querySelector('#lista_resultado')
+
 
 
